7b63719902e3a47d97423477f6eb25c7c1f9f70b
NikitaKnyazev <nikitaknyazev@yandex.ru>
2020-12-13 20:52:00 +0300

    update structure

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,25 @@
-FROM python:3
-COPY . /app
+FROM python:3.7
+
 WORKDIR /app
-RUN pip install flask \
+ADD . /app
+
+RUN pip install -r requirements.txt
+
+CMD ["python", "index.py"]
+
+
+
+
+#RUN pip install flask \
 #&& pip install numpy \
 #&& pip install torch \
 #&& pip install torchvision \
-&& pip install Pillow \
+#&& pip install Pillow \
 #&& pip install dominate \
-&& pip install youtube_dl \
-&& pip install moviepy
+#&& pip install youtube_dl \
+#&& pip install moviepy
 #&& pip install opencv-python \
 #&& pip install tqdm \
 #&& pip install cmake \
 #&& pip install dlib \
 #&& pip install scikit-image
-CMD ["python", "index.py"]
--- index.py
+++ index.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+#docker build -t myapp
+#docker run -d -p 5000:5000 myapp
+
 import os, shutil
 import subprocess
 from flask import Flask, render_template, request
@@ -6,11 +9,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    '''
     path = os.getcwd()+'/Results/TestVideoResults/'
     for dir in os.listdir(path):
         for file in os.listdir(path+'/'+dir):
             os.remove(path+'/'+dir+'/'+file)
             #print(path+'/'+dir+'/'+file)
+            '''
     return render_template('index.html')
 
 @app.route('/form', methods=['GET', 'POST'])
@@ -19,19 +24,14 @@ def form():
     time1 = request.args.get('timecod1')
     time2 = request.args.get('timecod2')
 
-    print(url)
-    print(time1)
-    print(time2)
-
-    file_path=os.getcwd()+"/test_FaceDict.py --gpu_ids -1 --source_url "+url+" --start "+time1+" --stop "+time2
+    #file_path=os.getcwd()+"/test_FaceDict.py --gpu_ids -1 --source_url "+url+" --start "+time1+" --stop "+time2
     #file_path="test.py "+url+" "+time1
-    os.system(f'py {file_path}')
+    #os.system(f'py {file_path}')
     return render_template('form2.html')
 
-@app.route('/result', methods=['GET', 'POST'])
+@app.route('/result', methods=['POST'])
 def result():
-    p = os.getcwd()
-    subprocess.call(p+"/templates/video.html", shell=True)
+    #subprocess.call("app/templates/video.html", shell=True)
     return render_template('form.html')
 
 if __name__ == '__main__':
