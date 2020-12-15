317c9dedcc3e8705da004de9cb267c5d53ff19f1
Tim882 <55451353+Tim882@users.noreply.github.com>
2020-12-15 14:36:04 +0300

    Add files via upload

--- index.py
+++ index.py
@@ -1,38 +1,22 @@
 # -*- coding: utf-8 -*-
-#docker build -t myapp
-#docker run -d -p 5000:5000 myapp
 
-import os, shutil
-import subprocess
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
+
+from test_FaceDict import process_video
+
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['POST'])
 def index():
-    '''
-    path = os.getcwd()+'/Results/TestVideoResults/'
-    for dir in os.listdir(path):
-        for file in os.listdir(path+'/'+dir):
-            os.remove(path+'/'+dir+'/'+file)
-            #print(path+'/'+dir+'/'+file)
-            '''
-    return render_template('index.html')
+    data = request.form
+    source_url = data['source_url']
+    target_start = data['target_start']
+    target_end = data['target_end']
+    process_video(source_url, target_start, target_end)
 
-@app.route('/form', methods=['GET', 'POST'])
-def form():
-    url = request.args.get('url')
-    time1 = request.args.get('timecod1')
-    time2 = request.args.get('timecod2')
+    return make_response()
 
-    file_path="/app/DFDNet/test_FaceDict.py"
-    #file_path="test.py "+url+" "+time1
-    os.system(f'py {file_path}')
-    return render_template('form2.html')
-
-@app.route('/result', methods=['POST'])
-def result():
-    #subprocess.call("app/templates/video.html", shell=True)
-    return render_template('form.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=4000)
