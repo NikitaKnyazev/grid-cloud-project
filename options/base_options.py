d69f3e5457a171f2a8d52ed842c0241d1f991533
nikitaknyazev <nikitaknyazev@yandex.ru>
2020-12-17 15:41:09 +0300

    f

--- options/base_options.py
+++ options/base_options.py
@@ -91,13 +91,16 @@ class BaseOptions():
 
         # set gpu ids
         str_ids = opt.gpu_ids.split(',')
-        opt.gpu_ids = []
+        opt.gpu_ids = -1#[]
+        '''
         for str_id in str_ids:
             id = int(str_id)
             if id >= 0:
                 opt.gpu_ids.append(id)
+
         if len(opt.gpu_ids) > 0:
             torch.cuda.set_device(opt.gpu_ids[0])
+            '''
 
         self.opt = opt
         return self.opt
