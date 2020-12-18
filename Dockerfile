9ad3ad1057bf4904b7f685d3d8cbf39dc026066e
nikitaknyazev <nikitaknyazev@yandex.ru>
2020-12-18 09:33:00 +0300

    rel

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-#FROM python:3.7
-FROM nvidia/cuda:10.2-base
-CMD nvidia-smi
+FROM python:3.7
+#FROM nvidia/cuda:10.2-base
+#CMD nvidia-smi
 
 
 #RUN apt-get update && apt-get install --no-install-recommends --no-install-suggests -y curl
@@ -8,12 +8,12 @@ CMD nvidia-smi
 #RUN apt-get -y install python3.7
 #RUN apt-get -y install python3.7-pip
 
-#WORKDIR /app
-#ADD . /app
-#RUN pip install -U pip
-#RUN pip install cmake
-#RUN pip install dlib
-#RUN apt-get update ##[edited]
-#RUN apt-get install ffmpeg libsm6 libxext6  -y
-#RUN pip install -r requirements.txt
-#CMD ["python", "index.py"]
+WORKDIR /app
+ADD . /app
+RUN pip install -U pip
+RUN pip install cmake
+RUN pip install dlib
+RUN apt-get update ##[edited]
+RUN apt-get install ffmpeg libsm6 libxext6  -y
+RUN pip install -r requirements.txt
+CMD ["python", "index.py"]
--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-import os, shutil
+import os, shutil, subprocess
 from flask import Flask, render_template, request, send_file
-from test_FaceDict import process_video
+#from FaceDict import process_video
 
 app = Flask(__name__)
 
@@ -18,16 +18,16 @@ def form():
     url = request.args.get('url')
     time1 = request.args.get('timecod1')
     time2 = request.args.get('timecod2')
-    process_video(url, time1, time2)
+    os.system('python FaceDict.py --url %s --time1 %s --time2 %s' % (url, time1, time2))
     return render_template('form2.html')
 
 @app.route('/Results/TestVideoResults/Step6_FinalVideo/result.mp4', methods=['GET', 'POST'])
 def video_result():
-    return send_file('Results/TestVideoResults/Step6_FinalVideo/result.mp4', as_attachment=True)
+    return send_file('./Results/TestVideoResults/Step1_FinalVideo/result.mp4', as_attachment=True)
 
 @app.route('/templates/video.png', methods=['GET', 'POST'])
 def img():
-    return send_file('templates/video.png', as_attachment=True)
+    return send_file('./templates/video.png', as_attachment=True)
 
 @app.route('/result', methods=['GET','POST'])
 def result():
